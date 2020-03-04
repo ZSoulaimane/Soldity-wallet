8d7a498cd1a98f19764135f60e23eb8f3542da1d
ZSoulaimane <zar.souler@gmail.com>
2020-03-05 00:54:10 +0100

    first commit

---
+++ Wallet.sol
@@ -0,0 +1,52 @@
+pragma solidity ^0.5.13;
+
+import "github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.4.0/contracts/ownership/Ownable.sol";
+import "github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.4.0/contracts/math/SafeMath.sol";
+
+contract Allowance is Ownable{
+    
+    using SafeMath for uint;
+    
+    event AllowanceChanged(address indexed _forWho, address indexed _fromwhom,uint _oldAmount ,uint _newAmount);
+    
+    mapping(address => uint) public allowance;
+    
+    function addAllowance(address _who, uint _amount) public onlyOwner {
+        emit AllowanceChanged(_who , msg.sender, allowance[_who], _amount);
+        allowance[_who] = _amount;
+    }
+    
+    modifier ownerOrAllowed(uint _amount) {
+        require(isOwner() || allowance[msg.sender] >= _amount, "You are not allowed");
+        _;
+    }
+    
+    function reduceAllowance(address _who,uint _amount) internal {
+        emit AllowanceChanged(_who, msg.sender,allowance[_who],allowance[_who]-_amount);
+        allowance[_who] = allowance[_who].sub(_amount);
+    }
+}
+
+contract simpleWallet is Allowance{
+
+    event MoneySent(address indexed _beneficiary, uint _amount);
+    event MoneyReceived(address indexed _from, uint _amount);
+
+    function withdrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount){
+        require(_amount <= address(this).balance , "there are not enough funds stored in the smart contract");
+        if(!isOwner()){
+            reduceAllowance(msg.sender, _amount);
+        }
+        emit MoneySent(_to, _amount);
+        _to.transfer(_amount);
+    }
+    
+    function renounceOwnership() public onlyOwner{
+        revert("can't renounce ownership here");
+    }
+    
+    function() external payable {
+        emit MoneyReceived(msg.sender,msg.value);
+    }
+    
+}
